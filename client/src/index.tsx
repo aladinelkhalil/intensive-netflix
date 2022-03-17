be0e15001c2776f60699e1b42ee8d555a061781b
Aladin El-Khalil <aladin@intensivecode.se>
2022-03-17 13:34:09 +0100

    Updated App component and added css reset

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,5 @@
 import ReactDOM from "react-dom";
+import App from "App";
+import "index.css";
 
-ReactDOM.render(<h1>Välkommen</h1>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
