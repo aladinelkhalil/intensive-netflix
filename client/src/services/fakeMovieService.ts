f9c635ffe74fbc35d8ddb50c2e40a41277317525
Aladin El-Khalil <aladin@intensivecode.se>
2022-03-17 13:32:26 +0100

    Created a temporary fakeMovieService

---
+++ client/src/services/fakeMovieService.ts
@@ -0,0 +1,38 @@
+import { Movie } from "types";
+
+const movies: Movie[] = [
+  {
+    _id: "1",
+    title: "The Shawshank Redemption",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg",
+    genre: {
+      _id: "10",
+      name: "Drama",
+    },
+  },
+  {
+    _id: "2",
+    title: "The Godfather",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UY720_.jpg",
+    genre: {
+      _id: "11",
+      name: "Crime",
+    },
+  },
+  {
+    _id: "3",
+    title: "The Dark Knight",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UY720_.jpg",
+    genre: {
+      _id: "12",
+      name: "Action",
+    },
+  },
+];
+
+export function getMovies() {
+  return movies;
+}
